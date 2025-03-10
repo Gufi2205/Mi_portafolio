0172c5bf8e50d7fa591d3e5d64b27fe73065a012
Gufi2205 <etzio2205perez@gmail.com>
2025-03-10 18:17:49 -0500

    correcion de links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -83,18 +83,31 @@ const About = () => {
                                 </div>
                             </a>
                             <a
-                                href="https://www.linkedin.com/in/julio-pérez-b687261a0"
+                                href="https://www.linkedin.com/in/julio-p%C3%A9rez-b687261a0" // URL corregida
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="group relative px-6 py-3 overflow-hidden rounded-lg bg-black border border-green-500 text-white transition-all duration-300"
                             >
+                                {/* Efecto de fondo al hacer hover */}
                                 <div className="absolute inset-0 w-0 bg-green-500 transition-all duration-300 group-hover:w-full opacity-20"></div>
+
+                                {/* Línea decorativa inferior */}
                                 <div className="absolute bottom-0 left-0 right-0 h-1 bg-green-400"></div>
                                 <div className="absolute bottom-0 left-0 right-0 h-1 bg-green-400 opacity-50 blur-sm"></div>
+
+                                {/* Contenido del botón */}
                                 <div className="relative flex items-center">
-                                    <svg className="w-5 h-5 mr-2 text-green-400 group-hover:text-green-300 transition-colors" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    {/* Icono de LinkedIn */}
+                                    <svg
+                                        className="w-5 h-5 mr-2 text-green-400 group-hover:text-green-300 transition-colors"
+                                        fill="currentColor"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                    >
                                         <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                                     </svg>
+
+                                    {/* Texto del botón */}
                                     <span className="group-hover:text-green-300 transition-colors">LinkedIn</span>
                                 </div>
                             </a>
